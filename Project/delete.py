dcf41c6c57de0f18b5341349eded7b852c6d24a4
Ken Thumi <kenthumi@gmail.com>
2021-04-10 06:00:12 +0300

    Remove Delete.py file

--- Project/delete.py
+++
@@ -1,9 +0,0 @@
-dict = {'name': {'password':'pwd','fullname':'thums'}}
-
-#dict = dict.fromkeys(['name2'],{'password':'pwd','fullname':'thums'})
-
-
-dict['name2'] = {'password':'pwd','fullname':'thums'}
-
-
-print(dict)
